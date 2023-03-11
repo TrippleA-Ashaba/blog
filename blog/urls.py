433f285320ff971fae86c44de67fad29eb7bc6ec
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-03-11 16:25:24 +0300

    can render pages with views

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import home_view, tutorial_view
+from .views import home_view, tutorial_view, single_post_view, about_view
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("tutorial/", tutorial_view, name="tutorial"),
+    path("tales/", tutorial_view, name="tales"),
+    path("post", single_post_view, name="post"),
+    path("about/", about_view, name="about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,15 @@ def home_view(request):
 
 def tutorial_view(request):
     return render(request, "blog/tutorial.html")
+
+
+def tales_view(request):
+    return render(request, "blog/tutorial.html")
+
+
+def single_post_view(request):
+    return render(request, "blog/single_post.html")
+
+
+def about_view(request):
+    return render(request, "blog/about.html")
