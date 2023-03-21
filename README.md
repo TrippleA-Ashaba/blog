0e50d0d3ae5267ed70098069e108fa4c923701b3
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-03-21 17:11:11 +0300

    untrack ignored files

--- README.md
+++
@@ -1,1 +0,0 @@
-New and improved blog from [ashabaheebwa](https://github.com/TrippleA-Ashaba/ashabaheebwa)
--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-
-# from .views import SignUpView
-
-# urlpatterns = [
-#     path("signup/", SignUpView.as_view(), name="signup"),
-#     # path("password/", AccountPasswordChangeView.as_view(), name="password_change"),
-# ]
